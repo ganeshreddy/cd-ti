fa0c24f2dba2205246b0dd7addce881b29581a7f
ganeshreddy <ganeshkv74@gmail.com>
2016-12-03 14:50:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -37,17 +37,16 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("room-zone")
 ###################################
-	spaceId = '18184'
-	apiToken = 'NkPxt41IvOLJC80dhKYsuWy0JGRB7wSZRKlbU3MSPSbkTrOtI5iO7caLbtaZQg1LPMIqoYFaMagpFgVu5370Mzjv5JUrdUf1yL2HdGSUW3lL1XaaSs8VMLeaZlz8hyIm'
+    spaceId = '18184'
+    apiToken = 'NkPxt41IvOLJC80dhKYsuWy0JGRB7wSZRKlbU3MSPSbkTrOtI5iO7caLbtaZQg1LPMIqoYFaMagpFgVu5370Mzjv5JUrdUf1yL2HdGSUW3lL1XaaSs8VMLeaZlz8hyIm'
 
 #url = 'https://api.robinpowered.com/v1.0/spaces/{}/presence'.format(spaceId)
 	url = 'https://api.robinpowered.com/v1.0/free-busy/spaces?location_ids=4495'
 # View all the presence in the space
-	response = requests.get( 
+    response = requests.get( 
 		url,
-		#headers={'content-type':'application/json', 'Authorization': 'Access-Token {}'.format(apiToken)}
 		headers={'content-type':'application/json', 'Authorization': 'Access-Token {}'.format(apiToken)}
-		)
+	)
 
 	print(response.json())
 ###################################
