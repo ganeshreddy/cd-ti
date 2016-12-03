d09d65c56f7d904c9aca56ab8b03618fa9ed6955
ganeshreddy <ganeshkv74@gmail.com>
2016-12-03 14:54:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,14 +41,14 @@ def makeWebhookResult(req):
     apiToken = 'NkPxt41IvOLJC80dhKYsuWy0JGRB7wSZRKlbU3MSPSbkTrOtI5iO7caLbtaZQg1LPMIqoYFaMagpFgVu5370Mzjv5JUrdUf1yL2HdGSUW3lL1XaaSs8VMLeaZlz8hyIm'
 
 #url = 'https://api.robinpowered.com/v1.0/spaces/{}/presence'.format(spaceId)
-	url = 'https://api.robinpowered.com/v1.0/free-busy/spaces?location_ids=4495'
+    url = 'https://api.robinpowered.com/v1.0/free-busy/spaces?location_ids=4495'
 # View all the presence in the space
     response = requests.get( 
 		url,
 		headers={'content-type':'application/json', 'Authorization': 'Access-Token {}'.format(apiToken)}
-	)
+    )
 
-	print(response.json())
+    print(response.json())
 ###################################
     
     cost = {'Large Conference Room':'Conference Room 27NA, Conference Room 27S, Conference Rm 28NA, Conference Rm 28', 
