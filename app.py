755f0d48a43e9f819d70f4998f7f69596b0edbf1
ganeshreddy <ganeshkv74@gmail.com>
2016-12-03 12:33:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,14 @@ def makeWebhookResult(req):
     spaceId = '18184'
     apiToken = 'NkPxt41IvOLJC80dhKYsuWy0JGRB7wSZRKlbU3MSPSbkTrOtI5iO7caLbtaZQg1LPMIqoYFaMagpFgVu5370Mzjv5JUrdUf1yL2HdGSUW3lL1XaaSs8VMLeaZlz8hyIm'
     url = 'https://api.robinpowered.com/v1.0/spaces/{}/presence'.format(spaceId)
+
+# View all the presence in the space
+    response = requests.get( 
+	    url,
+	    headers={'content-type':'application/json', 'Authorization': 'Access-Token {}'.format(apiToken)}
+	)
+
+    print(response.json())
 ###################################
     
     cost = {'Large Conference Room':'Conference Room 27NA, Conference Room 27S, Conference Rm 28NA, Conference Rm 28', 
@@ -55,6 +63,7 @@ def makeWebhookResult(req):
     
     print("Response:")
     print(speech)
+    print(response.json())
 
     return {
         "speech": speech,
