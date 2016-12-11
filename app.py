cb279a5dca909b03b0d041176844c712c323582a
ganeshreddy <ganeshkv74@gmail.com>
2016-12-11 10:58:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def makeWebhookResult(req):
         for rows in val['data']:
             retntxt= zone + ' ' + retntxt + rows['space']['type'] + ' type - ' + rows['space']['name'] + ' (' + str(rows['space']['capacity']) + ' person capacity)'  + ', Location Id ' + str(rows['space']['location_id']) + ', Space Id ' + str(rows['space']['id']) + '\n'        
         
-            retntxt = r.get_json()    
+            retntxt = r.json()    
             print retntxt
         
     return {
