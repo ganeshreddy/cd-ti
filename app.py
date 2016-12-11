9f4eb5da18b9dfd1ec4eba3b5715398bc6e2db3c
ganeshreddy <ganeshkv74@gmail.com>
2016-12-10 22:48:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,44 +67,6 @@ def makeWebhookResult(req):
             "source": "apiai-roombooking"
         }
 
- """   if req.get("result").get("action") != "room.book":
-        return {}    
-        result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("room-zone")
-    #zone = 'Conference Room'   
-    spaceId = '4495'
-    apiToken = 'NkPxt41IvOLJC80dhKYsuWy0JGRB7wSZRKlbU3MSPSbkTrOtI5iO7caLbtaZQg1LPMIqoYFaMagpFgVu5370Mzjv5JUrdUf1yL2HdGSUW3lL1XaaSs8VMLeaZlz8hyIm'
-
-    url = 'https://api.robinpowered.com/v1.0/free-busy/spaces?include=state&location_ids='+ spaceId
-    # View all the presence in the space
-    r = requests.get( 
-        url,
-        headers={'content-type':'application/json', 'Authorization': 'Access-Token {}'.format(apiToken)}
-        )
-    val = json.loads(r.text)
-
-    retntxt = ''
-    if val['data'] == []:
-        print 'No Data!'
-    else:
-        for rows in val['data']:
-            if zone.lower() in str(rows['space']['name'].lower()):
-                retntxt= zone + ' ' + retntxt + rows['space']['type'] + ' type - ' + rows['space']['name'] + ' (' + str(rows['space']['capacity']) + ' person capacity)'  + ', Location Id ' + str(rows['space']['location_id']) + ', Space Id ' + str(rows['space']['id']) + '\n'        
-        
-                print retntxt
-        
-    return {
-            "speech": retntxt,
-            "displayText": retntxt,
-            #"data": {},
-            # "contextOut": [],
-            "source": "apiai-roombooking"
-        }
-"""    
-
-   
-    
 
 
 if __name__ == '__main__':
