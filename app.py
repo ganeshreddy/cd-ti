8797d7fd2093558badb8adb4fd4d8e15aa554b12
ganeshreddy <ganeshkv74@gmail.com>
2016-12-10 22:06:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,42 +31,42 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "room.availability":
+    if req.get("result").get("action") in("room.availability", "room.book"):
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
     zone = parameters.get("room-zone")
-    #zone = 'Conference Room'	
+    #zone = 'Conference Room'   
     spaceId = '4495'
     apiToken = 'NkPxt41IvOLJC80dhKYsuWy0JGRB7wSZRKlbU3MSPSbkTrOtI5iO7caLbtaZQg1LPMIqoYFaMagpFgVu5370Mzjv5JUrdUf1yL2HdGSUW3lL1XaaSs8VMLeaZlz8hyIm'
 
     url = 'https://api.robinpowered.com/v1.0/free-busy/spaces?include=state&location_ids='+ spaceId
     # View all the presence in the space
     r = requests.get( 
-	    url,
-	    headers={'content-type':'application/json', 'Authorization': 'Access-Token {}'.format(apiToken)}
-	    )
+        url,
+        headers={'content-type':'application/json', 'Authorization': 'Access-Token {}'.format(apiToken)}
+        )
     val = json.loads(r.text)
 
     retntxt = ''
-#    zone = 'Desk'	
+#    zone = 'Desk'  
     
     if val['data'] == []:
         print 'No Data!'
     else:
         for rows in val['data']:
-		if zone.lower() in str(rows['space']['name'].lower()):
-		    retntxt= zone + ' ' + retntxt + rows['space']['type'] + ' type - ' + rows['space']['name'] + ' (' + str(rows['space']['capacity']) + ' person capacity)'  + ', Location Id ' + str(rows['space']['location_id']) + ', Space Id ' + str(rows['space']['id']) + '\n'        
+        if zone.lower() in str(rows['space']['name'].lower()):
+            retntxt= zone + ' ' + retntxt + rows['space']['type'] + ' type - ' + rows['space']['name'] + ' (' + str(rows['space']['capacity']) + ' person capacity)'  + ', Location Id ' + str(rows['space']['location_id']) + ', Space Id ' + str(rows['space']['id']) + '\n'        
         
-	            print retntxt
-		
+                print retntxt
+        
     return {
             "speech": retntxt,
             "displayText": retntxt,
             #"data": {},
             # "contextOut": [],
             "source": "apiai-roombooking"
-	    }
+        }
 
 
 if __name__ == '__main__':
