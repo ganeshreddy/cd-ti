05358723108b93fbcc32a642c71feb1638269fd6
ganeshreddy <ganeshkv74@gmail.com>
2016-12-10 23:22:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -73,27 +73,20 @@ def makeWebhookResult(req):
     result = req.get("result")
     parameters = result.get("parameters")
     zone = parameters.get("room-zone")
+    spaceid = parameters.get("spaceid")
     #zone = 'Conference Room'   
     spaceId = '4495'
     apiToken = 'NkPxt41IvOLJC80dhKYsuWy0JGRB7wSZRKlbU3MSPSbkTrOtI5iO7caLbtaZQg1LPMIqoYFaMagpFgVu5370Mzjv5JUrdUf1yL2HdGSUW3lL1XaaSs8VMLeaZlz8hyIm'
 
-    url = 'https://api.robinpowered.com/v1.0/free-busy/spaces?include=state&location_ids='+ spaceId
+    url = 'ttps://api.robinpowered.com/v1.0/spaces/'+ spaceId + '/events'
     # View all the presence in the space
-    r = requests.get( 
+    r = requests.post( 
         url,
         headers={'content-type':'application/json', 'Authorization': 'Access-Token {}'.format(apiToken)}
         )
-    val = json.loads(r.text)
-
-    retntxt = ''
-    if val['data'] == []:
-        print 'No Data!'
-    else:
-        for rows in val['data']:
-            if zone.lower() in str(rows['space']['name'].lower()):
-                retntxt= zone + ' ' + retntxt + rows['space']['type'] + ' type - ' + rows['space']['name'] + ' (' + str(rows['space']['capacity']) + ' person capacity)'  + ', Location Id ' + str(rows['space']['location_id']) + ', Space Id ' + str(rows['space']['id']) + '\n'        
-        
-                print retntxt
+    
+    retntxt = r.text
+    print retntxt
         
     return {
             "speech": retntxt,
